2fc6a58201c68d02b162610702e015517fe15425
unknown <mclaborn2010@gmail.com>
2023-05-10 13:39:11 -0500

    added state for animal count

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import {useState} from 'react';
+
+
+
 function App(){
-    return <div>Show animal list here</div>
+    const [count, setCount] = useState(0);
+
+
+    const handleClick = () =>{
+        setCount(count+1)
+    }
+    const clearAnimals = () => {
+        setCount(0)
+    }
+    return <div>
+        <button onClick={handleClick}>Add Animal</button>
+        <button onClick={clearAnimals}>Clear Animals</button>
+        <div>Number of animals: {count}</div>
+
+    </div>
 }
 
 export default App;
